302022f7e38b1f154ee940134489b45a746f7b5a
Max Strikanov <maxvell.88@mail.ru>
2020-08-19 13:42:08 +0300

    Урок №10

---
+++ index.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const btnCalc = document.getElementById('start'); 
+const btnAddAmount = document.getElementsByTagName('button')[0];
+const btnAddExpenses = document.getElementsByTagName('button')[1];
+const depositCheck = document.querySelector('#deposit-check');
+const additionalIncomeItem = document.querySelectorAll('.additional_income-item');
+const budgetMonthValue = document.getElementsByClassName('budget_month-value');
+const budgetDayValue = document.getElementsByClassName('budget_day-value');
+const expensesMonthValue = document.getElementsByClassName('expenses_month-value');
+const additionalIncomeValue = document.getElementsByClassName('additional_income-value');
+const additionalExpensesValue = document.getElementsByClassName('additional_expenses-value');
+const incomePeriodValue = document.getElementsByClassName('income_period-value');
+const targetMonthValue = document.getElementsByClassName('target_month-value');  
+const salaryAmount = document.querySelector('.salary-amount');
+const incomeTitle = document.querySelector('.income-title');
+const incomeAmount = document.querySelector('.income-amount');
+const expensesTitle = document.querySelector('.expenses-title');
+const expensesAmount = document.querySelector('.expenses-amount');
+const targetAmount = document.querySelector('.target-amount ');
+const periodSelect = document.querySelector('.period-select');
+
+console.log(btnCalc);
+console.log(btnAddAmount);
+console.log(btnAddExpenses);
+console.log(budgetMonthValue);
+console.log(budgetDayValue);
+console.log(expensesMonthValue);
+console.log(additionalIncomeValue);
+console.log(additionalExpensesValue);
+console.log(incomePeriodValue);
+console.log(targetMonthValue);
+console.log(salaryAmount);
+console.log(incomeTitle);
+console.log(incomeAmount);
+console.log(expensesTitle);
+console.log(expensesAmount);
+console.log(targetAmount);
+console.log(periodSelect);
+
+
