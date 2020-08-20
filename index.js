44956a8403c810cbe7b183d03702263dd3dfce72
Maksim Strikanov <59795160+MaxStrikanov@users.noreply.github.com>
2020-08-20 10:07:58 +0300

    Внес правки

--- index.js
+++ index.js
@@ -5,20 +5,41 @@ const btnAddAmount = document.getElementsByTagName('button')[0];
 const btnAddExpenses = document.getElementsByTagName('button')[1];
 const depositCheck = document.querySelector('#deposit-check');
 const additionalIncomeItem = document.querySelectorAll('.additional_income-item');
-const budgetMonthValue = document.getElementsByClassName('budget_month-value');
-const budgetDayValue = document.getElementsByClassName('budget_day-value');
-const expensesMonthValue = document.getElementsByClassName('expenses_month-value');
-const additionalIncomeValue = document.getElementsByClassName('additional_income-value');
-const additionalExpensesValue = document.getElementsByClassName('additional_expenses-value');
-const incomePeriodValue = document.getElementsByClassName('income_period-value');
-const targetMonthValue = document.getElementsByClassName('target_month-value');  
+const budgetMonthValue = document.getElementsByClassName('budget_month-value')[0];
+const budgetDayValue = document.getElementsByClassName('budget_day-value')[0];
+const expensesMonthValue = document.getElementsByClassName('expenses_month-value')[0];
+const additionalIncomeValue = document.getElementsByClassName('additional_income-value')[0]
+const additionalExpensesValue = document.getElementsByClassName('additional_expenses-value')[0]
+const incomePeriodValue = document.getElementsByClassName('income_period-value')[0];
+const targetMonthValue = document.getElementsByClassName('target_month-value')[0];  
 const salaryAmount = document.querySelector('.salary-amount');
-const incomeTitle = document.querySelector('.income-title');
+const incomeTitle = document.querySelector('.income-items [placeholder=Наименование]');
 const incomeAmount = document.querySelector('.income-amount');
-const expensesTitle = document.querySelector('.expenses-title');
+const expensesTitle = document.querySelector('.expenses-items [placeholder=Наименование]');
 const expensesAmount = document.querySelector('.expenses-amount');
+const additionalExpensesItem = document.querySelector('.additional_expenses-item');
+const depositBank = document.querySelector('.deposit-bank');
 const targetAmount = document.querySelector('.target-amount ');
 const periodSelect = document.querySelector('.period-select');
 
+console.log(btnCalc);
+console.log(btnAddAmount);
+console.log(btnAddExpenses);
+console.log(budgetMonthValue);
+console.log(budgetDayValue);
+console.log(expensesMonthValue);
+console.log(additionalIncomeValue);
+console.log(additionalExpensesValue);
+console.log(incomePeriodValue);
+console.log(targetMonthValue);
+console.log(salaryAmount);
+console.log(incomeTitle);
+console.log(incomeAmount);
+console.log(expensesTitle);
+console.log(expensesAmount);
+console.log(additionalExpensesItem);
+console.log(depositBank);
+console.log(targetAmount);
+console.log(periodSelect);
 
 
